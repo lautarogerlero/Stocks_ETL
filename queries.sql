83f1fddd526bf6888891ee60d9cbd314de30771f
lautarogerlero <lautarogerlero@gmail.com>
2024-02-12 14:14:59 -0300

    PK

--- queries.sql
+++ queries.sql
@@ -1,14 +1,11 @@
-create table stocks (
-	symbol VARCHAR(10),
-	date DATE,
-	open_value DECIMAL(10,2),
-	high_value DECIMAL(10,2),
-	low_value DECIMAL(10,2),
-	close_value DECIMAL(10,2),
-	volume INT
+CREATE TABLE stocks (
+    symbol VARCHAR(10),
+    date DATE,
+    open_value DECIMAL(10,2),
+    high_value DECIMAL(10,2),
+    low_value DECIMAL(10,2),
+    close_value DECIMAL(10,2),
+    volume INT,
+    ingestion_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (ingestion_timestamp)
 );
-
-ALTER TABLE stocks
-ADD COLUMN ingestion_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
-
-select * from stocks;
